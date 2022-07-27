28c0fc8484aa68d968d4aabc1b0086733cc00e8b
IAXRetailer <retailer673@163.com>
2022-07-27 10:51:45 +0800

    add uuid4

--- Library/IQtTool/IconTool.py
+++ Library/IQtTool/IconTool.py
@@ -1,9 +1,10 @@
 import ctypes
 from PyQt5.QtGui import QIcon
+from uuid import uuid4
 boxicon="Data/box.png"
 def setIcon(self):
     try:
         self.setWindowIcon(QIcon(boxicon))
-        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("com.github.iaxretailer.saltzip.mainwindows")
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("com.github.iaxretailer.box.%s"%uuid4())
     except:
         pass
